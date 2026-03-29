4e89e118fefafc705082d07111547ccf5b885156
josefhint755-netizen <josefhint755@gmail.com>
2026-03-30 00:51:47 +0700

    commit message

--- TP_MODUL5_103022400006/Program.cs
+++ TP_MODUL5_103022400006/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL5_103022400006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+
+            halo.SapaUser("Josefhint");
+
+            DataGeneric<string> data = new DataGeneric<string>("103022400006");
+            data.PrintData();
+        }
+    }
+
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine("Halo user " + x);
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
